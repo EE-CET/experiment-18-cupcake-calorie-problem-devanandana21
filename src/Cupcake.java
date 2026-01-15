3d0cc5fde9e58b4fa7b569957610f4326018b157
devanandana21 <tve24el018@cet.ac.in>
2026-01-15 19:31:54 +0530

    Update Cupcake.java

--- src/Cupcake.java
+++ src/Cupcake.java
@@ -4,19 +4,30 @@ public class Cupcake {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr = new int[n];
 
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 
-        Arrays.sort(arr);
-
         long result = 0;
         int power = 0;
 
-        for (int i = n - 1; i >= 0; i--) {
-            result += (long) arr[i] * (1L << power);
+        boolean[] used = new boolean[n];
+
+        for (int count = 0; count < n; count++) {
+            int max = -1;
+            int idx = -1;
+
+            for (int i = 0; i < n; i++) {
+                if (!used[i] && arr[i] > max) {
+                    max = arr[i];
+                    idx = i;
+                }
+            }
+
+            used[idx] = true;
+            result += (long) max * (1L << power);
             power++;
         }
 
