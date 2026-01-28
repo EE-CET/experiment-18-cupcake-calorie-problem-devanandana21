52ad30dc353351d951020f472ee16fb82c15655b
devanandana21 <tve24el018@cet.ac.in>
2026-01-28 13:19:38 +0530

    Update Cupcake.java

--- src/Cupcake.java
+++ src/Cupcake.java
@@ -1,35 +1,29 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Cupcake{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
-        int[] calories = new int[n];
-        boolean[] used = new boolean[n];
+        long[] calories = new long[n];
 
         for (int i = 0; i < n; i++) {
-            calories[i] = sc.nextInt();
+            calories[i] = sc.nextLong();
         }
 
-        long totalMiles = 0;
-        int power = 0;
+        // Sort ascending
+        Arrays.sort(calories);
 
-        for (int count = 0; count < n; count++) {
-            int maxVal = -1;
-            int idx = -1;
+        long miles = 0;
+        long power = 1; // 2^0
 
-            for (int i = 0; i < n; i++) {
-                if (!used[i] && calories[i] > maxVal) {
-                    maxVal = calories[i];
-                    idx = i;
-                }
-            }
-
-            used[idx] = true;
-            totalMiles += (long) maxVal * (1L << power);
-            power++;
+        // Traverse from largest to smallest
+        for (int i = n - 1; i >= 0; i--) {
+            miles += calories[i] * power;
+            power *= 2;
         }
 
-        System.out.print(totalMiles);
+        System.out.println(miles);
+        sc.close();
     }
 }
